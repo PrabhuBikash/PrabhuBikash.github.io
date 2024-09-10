0a5b890ed6232d83175199d587774abd6edb3d52
PrabhuBikash <145930199+PrabhuBikash@users.noreply.github.com>
2024-09-10 08:54:02 +0530

    I had forgotten comma that made it crash and has to link to pdfData.js and commented onused places

--- NISER_Notes-main/pdfData.js
+++ NISER_Notes-main/pdfData.js
@@ -10,31 +10,31 @@ const footnote = document.getElementById("foot-note");
 const baseUrl = 'Notes'; // Base URL for PDF files
 
 const pdfData = {
-    Physics : {
-        'Mathematical methods I': [],
-    }
-    Chemistry : {
+    //Physics : {
+    //    'Mathematical methods I': [],
+    //},
+    //Chemistry : {
 
-    }
+    //},
     Mathematics : {
         'Discrete Mathematics': ['DM First Class (01-08-2024)','DM Notes (~Himanshu Sharma)'],
         'Real Analysis': ['RA First Class (01-08-2024)','Real Analysis 05-08-2024'],
         'Linear Algebra': ['Linear Algebra First Class','LA Exercises (upto 1st quiz)'],
-    }
-    Biology : {
+    },
+    //Biology : {
         
-    }
-    Humanities : {
-        'Introduction to psychology': [],
-        'Sociology of science and technology': []
-    }
+    //},
+    //Humanities : {
+    //    'Introduction to psychology': [],
+    //    'Sociology of science and technology': []
+    //},
     Computer : {
         'Theory of computation': ['TOC Assignment 0'],
-    }
-    Istyear : {
+    },
+    //Istyear : {
 
-    }
-    About : {
+    //},
+    //About : {
 
-    }
+    //},
 };
