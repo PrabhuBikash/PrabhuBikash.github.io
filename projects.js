965cee5057621eea8f804cc98c10b7f13a7f295e
PrabhuBikash <145930199+PrabhuBikash@users.noreply.github.com>
2026-03-17 12:52:02 +0530

    added 404 page and removed list of projects to a js file

---
+++ projects.js
@@ -0,0 +1,42 @@
+const projects = [
+  {
+    "name": "Detour",
+    "link": "detour",
+    "desc": "A game that tests you stability"
+  },
+  {
+    "name": "Code Stock Animation",
+    "link": "CodeStockAnimation",
+    "desc": "Just type your code in slides and get its stock animation"
+  },
+  {
+    "name": "Art to HTML",
+    "link": "ArtToHTML",
+    "desc": "Converts given image to HTML code"
+  },
+  {
+    "name": "Script Narrator",
+    "link": "ScriptNarrator",
+    "desc": "Make your script speak"
+  },
+  {
+    "name": "Sort It!",
+    "link": "SortIt",
+    "desc": "A purely 1D game"
+  },
+  {
+    "name": "Logic Simulators",
+    "link": "LogicSimulators",
+    "desc": "Collection of Logic Simulators"
+  },
+  {
+    "name": "Sequence Predictor",
+    "link": "SequencePredictor",
+    "desc": "Predicts next terms of given finite sequence"
+  },
+  {
+    "name": "Pattern Finder",
+    "link": "PatternFinder",
+    "desc": "Vaguely remeber your pattern? it may help you find it"
+  }
+]
